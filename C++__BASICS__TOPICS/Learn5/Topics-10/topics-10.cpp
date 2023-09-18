9799f48e67074499b8d3468f69e48f69535a5d53
protapsaha1 <webprotap09@gmail.com>
2023-09-19 03:49:52 +0600

    learn 5 topics 9 switch str. and if else

---
+++ C++__BASICS__TOPICS/Learn5/Topics-10/topics-10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = (0 + 1); i <= 100 || i / 2; i++)
+    {
+        cout << "result=" << i << endl;
+    }
+    return 0;
+}
