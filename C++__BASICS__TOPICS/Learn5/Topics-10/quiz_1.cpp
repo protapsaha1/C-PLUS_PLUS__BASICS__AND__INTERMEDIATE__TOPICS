f3126a10b7d43c6a2ee32e4c7651502effc4b1c5
protapsaha1 <webprotap09@gmail.com>
2023-09-20 02:05:31 +0600

    learn 5 topice 10 quiz

---
+++ C++__BASICS__TOPICS/Learn5/Topics-10/quiz_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     int tb = 6 * i;
+    //     cout << tb << endl;
+    // };
+
+    // int i = 1;
+    // while (i <= 10)
+    // {
+    //     int tb = 6 * i;
+    //     cout << tb << endl;
+    //     i++;
+    // }
+    
+    // int i = 1;
+    // do
+    // {
+    //     int tb = 6 * i;
+    //     i++;
+    //     cout << tb << endl;
+    // } while (i <= 10);
+
+    return 0;
+}
