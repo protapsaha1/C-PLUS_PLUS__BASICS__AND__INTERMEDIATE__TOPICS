4f90d735efdc37862f720828eaff21dbcfe8e1d8
protapsaha1 <webprotap09@gmail.com>
2023-09-20 01:51:42 +0600

    learn 5 topice 10 loops

--- C++__BASICS__TOPICS/Learn5/Topics-10/topics-10.cpp
+++ C++__BASICS__TOPICS/Learn5/Topics-10/topics-10.cpp
@@ -2,10 +2,38 @@
 using namespace std;
 int main()
 {
+    // normal for loop
+    // for (int i = (0 + 1); i <= 100; i++)
+    // {
+    //     cout << "result=" << i << endl;
+    // };
+    // infinity loop
+    // for (int i = (0 + 1); i <= 100 || i / 2; i++)
+    // {
+    //     cout << "result=" << i << endl;
+    // };
 
-    for (int i = (0 + 1); i <= 100 || i / 2; i++)
-    {
-        cout << "result=" << i << endl;
-    }
+    // While loop
+    int i = 1;
+    // while (i <= 30)
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+    // cout << "i am out " << i;
+
+    // infinity while loop
+    // while (i <= 30)
+    // {
+    //     cout << i << endl;
+    // };
+
+    // do while loop
+    // do
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // } while (i <= 30);
+
     return 0;
 }
